0791286ee7a23502820d41c4e3ce54c2a149b1a6
Bryce Haley <bhaley91@gmail.com>
2020-06-09 01:39:56 -0400

    consolidated saving and editing

--- notebooks/html_scripts/scripts/createCanvas.js
+++ notebooks/html_scripts/scripts/createCanvas.js
@@ -34,7 +34,6 @@ function updateTextCanvas(row, col) {
     "#F0E68C": "f",
     "#d5a967": "-"
   };
-
   textCanvas[row][col] = colourDict[selectedValue];
 }
 
@@ -103,24 +102,73 @@ function removeGrid() {
 function setGrid(){
   let rows = document.getElementById("height").value;
   let cols = document.getElementById("width").value;
+  if (rows < 5) {rows = 5;}
+  else if(rows >50) {rows = 50;}
+  if (cols < 5) {cols = 5;}
+  else if (cols > 50) {cols = 50;}
+  selectedID = "";
+  selectedValue = "";
   removeGrid();
   createDirList();
   textCanvas = makeGrid(rows,cols);
 }
 
+function removeOptions(fileSelect) {
+  let length = fileSelect.options.length;
+  console.log("length of list:", length);
+  for (let i = 0; i < length; i++) {
+    fileSelect.remove(i);
+    console.log("in remove list");
+  }
+}
 
+async function getFileDir() {
+  try{
+    const response = await fetch("patterns/dirList");
+    const data = await response.text();
+    const dirList = await data.split(", ");
+    console.log(dirList);
+    return dirList;
+  } catch {
+    console.error("failed to grab list");
+  }
+}
+
+async function updateFileList() {
+  try{
+    let dirList  = await getFileDir();
+    console.log(dirList);
+    if(dirList) {
+      console.log("true List");
+      let fileSelect = document.getElementById("file-list");
+      removeOptions(fileSelect);
+      for(elem in dirList){
+        console.log("elem:", dirList[elem]);
+        let opt = document.createElement("option");
+        opt.value = dirList[elem];
+        opt.innerHTML = dirList[elem];
+        fileSelect.appendChild(opt);
+      }
+    }
+  }catch{
+    console.error("failed to grab list");
+  }
+}
+
+/*Checks if a new patternfile has been created and updates
+ * the text next to the save button appropriatly
+ */
 async function isSaved(fileName){
   try{
     const checkResponse = await fetch("patterns/"+ fileName);
     const checkData = await checkResponse.text();
+    console.log(checkData);
+    console.log(typeof checkData);
     if(checkData) {
-      console.log("saved:");
       document.getElementById("status-text").innerHTML = "filename: '" + fileName + "' saved.";
-
     }
   } catch {
-    console.log("not saved");
-    document.getElementById("status-text").innerHTML = "filename: '" + fileName + "' already used";
+    document.getElementById("status-text").innerHTML = "try saving again.";
   }
 }
 
@@ -128,32 +176,102 @@ function save(fileName){
   let stringCanvas = canvasArrToString(textCanvas);
   let kernel = IPython.notebook.kernel;
   let command = "save_edited_pattern('" + fileName + "', '" + stringCanvas + "')";
-  console.log(command);
   kernel.execute(command);
 }
 
 async function saveAndDisplay(fileName) {
   try{
-    const response = await fetch("patterns/dirList");
-    const data = await response.text();
-    const dirList = data.split(", ");
+    const dirList = await getFileDir();
     const nameUsed = dirList.includes(fileName);
     if (nameUsed) {
-      console.log(fileName, "is in", dirList);
       document.getElementById("status-text").innerHTML = "filename: '" + fileName + "' already used";
     } else {
-      console.log(fileName, "is not in:", dirList);
       save(fileName);
       document.getElementById("status-text").innerHTML = "Saving...";
       setTimeout(function() { isSaved(fileName); }, 3000);
+      setTimeout(function() { updateFileList(); }, 3000);
     }
   } catch {
-    console.error("failed");
-    document.getElementById("status-text").innerHTML = "File failed to save./";
-
+    document.getElementById("status-text").innerHTML = "File failed to save.";
   }
 }
 
+function setColours(patternArr){
+  const colourDict = {
+    "a": "#000000",
+    "b": "#F5DEB3",
+    "c": "#800000",
+    "d": "#ffe07c",
+    "e": "#DAA520",
+    "f": "#F0E68C",
+    "-": "#d5a967"
+  };
+  console.log("in setColours");
+
+  for(let col=0; col<patternArr[0].length; col++){
+    for(let row=0; row<patternArr.length; row++){
+      let symbol = patternArr[row][col];
+      if (symbol!== "-") {
+        let index = row + "," + col
+        console.log(index, symbol);
+        document.getElementById(index).style.backgroundColor = colourDict[symbol];
+      }
+    }
+  }
+}
+
+function patternToArr(pattern) {
+  let patternArr = pattern.split("\n");
+  console.log(patternArr);
+  if (patternArr[patternArr.length -1] === "") {
+    patternArr.splice(-1,1);
+  }
+  console.log(patternArr);
+  let cols = patternArr[0].length;
+  let regular = patternArr.every((elem) => elem.length === cols);
+  if (!regular) {
+    return 0;
+  }
+  console.log("is reg");
+  patternArr = patternArr.map((x) => x.split(""));
+  console.log(patternArr);
+  return patternArr;
+}
+
+function updateCanvas(patternArr){
+  document.getElementById("paint-area").style.display = "none";
+  cols = patternArr[0].length;
+  rows = patternArr.length;
+  if(cols >= 5 && cols <= 51 && rows >=5 && rows <= 50){
+    selectedID = "";
+    selectedValue= "";
+    removeGrid();
+    makeGrid(rows,cols);
+    setColours(patternArr);
+
+  } else {
+    //print error
+  }
+}
+async function loadCanvas() {
+  let e = document.getElementById("file-list");
+  fileName = e.options[e.selectedIndex].value
+  console.log(fileName);
+  try {
+    const response = await fetch("patterns/"+ fileName);
+    const pattern = await response.text();
+    console.log(pattern);
+    patternToArr(pattern);
+    patternArr = patternToArr(pattern);
+    if(patternArr) {
+      updateCanvas(patternArr);
+    }else{
+      // put error message
+    }
+  } catch {
+    console.error("did not load");
+  }
+}
 
 document.getElementById("paint-area").style.display = "none";
 
@@ -176,3 +294,5 @@ document.querySelectorAll('.save-btn').forEach(function(e) {
     saveAndDisplay(fileName);
   })
 });
+
+updateFileList();
