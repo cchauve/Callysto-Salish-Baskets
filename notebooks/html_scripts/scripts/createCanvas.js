7fd1028e79007b6c2c45aa0988e9f043a69239d0
Bryce Haley <bhaley91@gmail.com>
2020-06-10 22:06:20 -0400

    comments added and bug fixed where AtomicSnake motif would not load

--- notebooks/html_scripts/scripts/createCanvas.js
+++ notebooks/html_scripts/scripts/createCanvas.js
@@ -7,12 +7,17 @@ var selectedValue = "";
 //holds the encoded pattern infromation("abcdef-") in an array of arrays
 var textCanvas = [[],[]];
 
+//updates the stylesheet to have a grid of dimnensions specified
 function setCSSGrid(rows, cols) {
   let canvas = document.getElementById("canvas");
   canvas.style.setProperty('--canvas-rows', rows);
   canvas.style.setProperty('--canvas-cols', cols);
 }
 
+// Called by makeGrid
+// input: dimensions of canvas
+// output: an array of specified size where each element is '-'
+//update the 2d array representation of the canvas when a canvas button is pressed
 function createTextCanvas(rows,cols) {
   var tempCanvas = [...Array(Number(rows)).keys()].map(e => Array(Number(cols)));
 
@@ -24,6 +29,11 @@ function createTextCanvas(rows,cols) {
   return tempCanvas;
 }
 
+
+// called by canvas buttons
+// input row  and column specifying the button
+// output: None
+// updates the array holding the encoded canvas pattern to show a change in the canvas
 function updateTextCanvas(row, col) {
   let colourDict = {
     "#000000": "a",
@@ -34,15 +44,19 @@ function updateTextCanvas(row, col) {
     "#F0E68C": "f",
     "#d5a967": "-"
   };
-  //console.log(textCanvas);
   textCanvas[row][col] = colourDict[selectedValue];
 }
 
+//makes canvas visible
 function showPaintArea() {
   let paintArea = document.getElementById("paint-area");
   paintArea.style.display = "block";
 }
 
+//called by setGrid and updateCanvas
+//input: dimensions of the array
+//output: an array of specified size where each element is '-'
+//Creates the blank encoded pattern
 function makeGrid(rows, cols) {
   setCSSGrid(rows,cols);
   let canvas = document.getElementById("canvas");
@@ -69,6 +83,9 @@ function makeGrid(rows, cols) {
   return createTextCanvas(rows, cols);
 }
 
+// called by save
+// input: array holding 'abcdef-' encoded patten
+// return: a string encoding the 'abcdef' pattern
 function canvasArrToString(arr) {
   let dims = [arr[0].length, arr.length];
   let stringCanvas = "";
@@ -82,68 +99,86 @@ function canvasArrToString(arr) {
   return stringCanvas;
 }
 
+// called by setGrid, loadCanvas and save button
+// input: none
+// output: none
+// Calls python code to create dirList file which lists all patterns in '/patterns'
 function createDirList() {
   let kernel = IPython.notebook.kernel;
   let command = "generate_pattern_dir_listing()";
   kernel.execute(command);
 }
 
+// called up updateCanavas and setGrid
+// input: none
+// output:none
+// removes all buttons from the grid
 function removeGrid() {
   [].forEach.call(document.querySelectorAll('.canvas-square'), function(e){
     e.parentNode.removeChild(e);
   });
 }
 
-/*setGrid creates all the variables needed for the canvas
- * is ran by the clicking the shape button
-  * */
+// Called by CreateBlank Canvas button
+// input: none
+// output: none
+// grabs the values selected for height and width and creates the grid of buttons
 function setGrid(){
   let rows = document.getElementById("height").value;
   let cols = document.getElementById("width").value;
-  if (rows < 5) {rows = 5;}
+
+  //clips input if out of range
+  if (rows < 4) {rows = 4;}
   else if(rows >50) {rows = 50;}
-  if (cols < 5) {cols = 5;}
+  if (cols < 4) {cols = 4;}
   else if (cols > 50) {cols = 50;}
+
+  //clears existing settings
   selectedID = "";
   selectedValue = "";
   removeGrid();
+
+  //updates screen with new settings
   createDirList();
   textCanvas = makeGrid(rows,cols);
 }
 
+//called by asyncUpdateFileList
+//input: the file select html element
+//output: none
+//removes all option from the dropdown menu
 function removeOptions(fileSelect) {
   let length = fileSelect.options.length;
-  //console.log("in removeOptions");
   for (let i = length-1; i>=0; i--) {
     fileSelect.remove(0);
-    //console.log("in remove list");
-    //console.log(fileSelect.options.length);
   }
 }
 
+// called throughout
+// input: none
+// output: a list of the files listed in '/patterns/dirList'
 async function getFileDir() {
   try{
     const response = await fetch("patterns/dirList");
     const data = await response.text();
     const dirList = await data.split(", ");
-    console.log(dirList);
     return dirList;
   } catch {
     console.error("failed to grab list");
   }
 }
 
+// called by updateFileList
+// input: none
+// output: none
+// get the current dirList file and update the dropdown menu with availble patterns
 async function asyncUpdateFileList() {
-  console.log("updatingFileLIst");;
   try{
     let dirList  = await getFileDir();
-    console.log(dirList);
     if(dirList) {
-      // console.log("true List");
       let fileSelect = document.getElementById("file-list");
       removeOptions(fileSelect);
       for(elem in dirList){
-        // console.log("elem:", dirList[elem]);
         let newOption = new Option(dirList[elem], dirList[elem]);
         fileSelect.add(newOption, undefined);
       }
@@ -153,20 +188,23 @@ async function asyncUpdateFileList() {
   }
 }
 
+//Called by the Refresh button
+// input: none
+// output: none
+// resets the canvas then 0.5 seconds later
 function updateFileList(){
   loadCanvas();
   setTimeout(function(){ asyncUpdateFileList(); }, 500);
 }
 
-/*Checks if a new patternfile has been created and updates
- * the text next to the save button appropriatly
- */
+// Called by saveAndDisplay 3 seconds after saving
+// input: fileName
+// output: none
+// writes out the save status beside the save button
 async function isSaved(fileName){
   try{
     const checkResponse = await fetch("patterns/"+ fileName);
     const checkData = await checkResponse.text();
-    //   console.log(checkData);
-    // console.log(typeof checkData);
     if(checkData) {
       document.getElementById("status-text").innerHTML = "filename: '" + fileName + "' saved.";
     }
@@ -175,6 +213,10 @@ async function isSaved(fileName){
   }
 }
 
+// Called by saveAndDisplay
+// input: the fileName selected
+// output:  none
+// Calls the python code to save the pattern
 function save(fileName){
   let stringCanvas = canvasArrToString(textCanvas);
   let kernel = IPython.notebook.kernel;
@@ -182,8 +224,13 @@ function save(fileName){
   kernel.execute(command);
 }
 
+// Called by the save button
+// input: the fileName specified by the dropdown
+// output: none
+// checks if the fileName is already in use, saves if not
 async function saveAndDisplay(fileName) {
   try{
+    //from the patternsFolder read the "dirList" file and check if the fileName is used
     const dirList = await getFileDir();
     const nameUsed = dirList.includes(fileName);
     if (nameUsed) {
@@ -198,6 +245,10 @@ async function saveAndDisplay(fileName) {
   }
 }
 
+// Called up updateCanvas
+// input: an 2d Array storing the 'abcdef-' code
+// output: none
+// updates button colours to reflect the pattern
 function setColours(patternArr){
   const colourDict = {
     "a": "#000000",
@@ -208,43 +259,54 @@ function setColours(patternArr){
     "f": "#F0E68C",
     "-": "#d5a967"
   };
-  //console.log("in setColours");
 
   for(let col=0; col<patternArr[0].length; col++){
     for(let row=0; row<patternArr.length; row++){
       let symbol = patternArr[row][col];
       if (symbol!== "-") {
         let index = row + "," + col
-        // console.log(index, symbol);
         document.getElementById(index).style.backgroundColor = colourDict[symbol];
       }
     }
   }
 }
 
+
+// Called by loadCanvas
+// input: a pattern string in "abcdef-"
+// output: the string in a 2d array format
 function patternToArr(pattern) {
   let patternArr = pattern.split("\n");
-  //console.log(patternArr);
+
   if (patternArr[patternArr.length -1] === "") {
     patternArr.splice(-1,1);
   }
-  // console.log(patternArr);
+
   let cols = patternArr[0].length;
   let regular = patternArr.every((elem) => elem.length === cols);
   if (!regular) {
     return 0;
   }
-  //console.log("is reg");
+  //turn each element of the array into an array
   patternArr = patternArr.map((x) => x.split(""));
-  //console.log(patternArr);
   return patternArr;
 }
 
+// called by loadCanvas
+// input: an array of 'abcdef-'
+// output: none
+// generates the new html grid
 function updateCanvas(patternArr){
+  console.log("in update Canvas");
+  //hides the current grid
+
+  console.log(patternArr);
   document.getElementById("paint-area").style.display = "none";
   let cols = patternArr[0].length;
   let rows = patternArr.length;
-  if(cols >= 5 && cols <= 51 && rows >=5 && rows <= 50){
+
+  //if the dimesions are okay, delete the old grid and display a new one
+  if(cols>=4 && cols<=51 && rows>=4 && rows<=50){
     selectedID = "";
     selectedValue= "";
     removeGrid();
@@ -252,32 +314,45 @@ function updateCanvas(patternArr){
     setColours(patternArr);
     textCanvas = patternArr;
   } else {
-    //print error
+    //TODO: print error
   }
 }
+
+//Called by Edit Canvas button
+//input: none
+//output: none
+//grabs the file selected and outputs it on the canvas below
 async function loadCanvas() {
+  // grab the file selected by user
   let e = document.getElementById("file-list");
   fileName = e.options[e.selectedIndex].value
-  //console.log(fileName);
   try {
+    //load the 'abcdef-' string from the motif file selected
     const response = await fetch("patterns/"+ fileName);
     const pattern = await response.text();
-    //console.log(pattern);
-    patternToArr(pattern);
     patternArr = patternToArr(pattern);
+
+    //update the dirList file in the pattern directory
     createDirList();
+
+    console.log(patternArr);
+    //if the patternfile is legitimate(dimensions agree)
     if(patternArr) {
+      console.log("patternArr is true");
       updateCanvas(patternArr);
+      console.log("canvas updated");
     }else{
-      // put error message
+      // TODO: put error message
     }
   } catch {
     console.error("did not load");
   }
 }
 
+//hide the canvas
 document.getElementById("paint-area").style.display = "none";
 
+//place onClick on the pallette buttons
 document.querySelectorAll('.colour-options').forEach(function(e) {
   e.addEventListener('click', function() {
     this.style.borderColor = "green";
@@ -289,8 +364,8 @@ document.querySelectorAll('.colour-options').forEach(function(e) {
   })
 });
 
+//placed onClick on the save button
 document.querySelectorAll('.save-btn').forEach(function(e) {
-
   e.addEventListener('click', function() {
     createDirList();
     let fileName = document.getElementById("file-name").value;
